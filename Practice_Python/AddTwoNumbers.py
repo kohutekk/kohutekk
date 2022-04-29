3c9ba3a611fcedb7b6807cb95709fef92eb6bb1b
kohutekk <104512863+kohutekk@users.noreply.github.com>
2022-04-29 17:53:33 -0500

    Update AddTwoNumbers.py

--- Practice_Python/AddTwoNumbers.py
+++ Practice_Python/AddTwoNumbers.py
@@ -1,26 +1,21 @@
 ## Add Two Numbers
+def my_function(arr):
+    a = len(arr) - 1
+    for x in range(len(arr)//2):
+        num = arr[x]
+        arr[x] = arr[a]
+        arr[a] = num
+        a -= 1
 
-n1 = (input("Enter in three single digit numbers, with a space between each one: ")).split();
-num = n1[0];
-n1[0] = n1[2];
-n1[2] = num;
-num1 = "".join(n1);
+n1 = (input("Enter in three single digit numbers, with a space between each one: ")).split()
+my_function(n1)
+num1 = "".join(n1)
 
-n2 = (input("Now, enter in three more single digit numbers, with a space between each one: ")).split();
-num = n2[0];
-n2[0] = n2[2];
-n2[2] = num;
-num2 = "".join(n2);
+n2 = (input("Now, enter in three more single digit numbers, with a space between each one: ")).split()
+my_function(n2)
+num2 = "".join(n2)
 
-ans = int(num1) + int(num2);
-answer = [int(i) for i in str(ans)];
-print (answer);
-
-#need a method to reverse string arrays
-
-#a = len(n1) - 1
-#for x in range(len(n1)//2):
-#    num = n1[x]
-#    n1[x] = n1[a]
-#    n1[a] = num
-#    a -= 1
+ans = int(num1) + int(num2)
+answer = [int(i) for i in str(ans)]
+my_function(answer)
+print (answer)
