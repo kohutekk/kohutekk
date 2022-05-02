03ded4866f3e2df137017ccc8f9d65b3cb26c277
kohutekk <104512863+kohutekk@users.noreply.github.com>
2022-05-02 11:05:34 -0500

    Update ZigZagConversion.py

--- Practice_Python/ZigZagConversion.py
+++ Practice_Python/ZigZagConversion.py
@@ -1,2 +1,15 @@
 ## Zigzag Conversion
 
+str = list(input("Enter in a String: "))
+numRow = int(input("Enter number of rows: "))
+totCycle = numRow + (numRow - 2)
+
+for x in range(numRow):
+    print(str[x], end='')
+    for y in range(numRow - 2):
+        print(" ", end='')
+    
+
+print (" ")
+print (str)
+print (numRow, " - ", totCycle)
