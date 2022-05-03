5f0a32bd870f2cf79816c629f16af79da9a32cfb
kohutekk <104512863+kohutekk@users.noreply.github.com>
2022-05-03 14:35:10 -0500

    Update ZigZagConversion.py

--- Practice_Python/ZigZagConversion.py
+++ Practice_Python/ZigZagConversion.py
@@ -1,28 +1,30 @@
 ## Zigzag Conversion
 
+#Functions to build each line
+def first_space(row, numRow):
+    for a in range(row, numRow - 2):
+        print("-", end='')
+        
+def second_space(row):
+    for a in range(row - 1):
+        print("-", end='')
+
+#Intro
 str = list(input("Enter in a String: "))
 numRow = int(input("Enter number of rows: "))
 totCycle = numRow + (numRow - 2)
-print (" ")
-
-for x in range(numRow):
-    print(x, end='')
-    for y in range(x, numRow - 2):
-        print("-", end='')
-    if x == (numRow-1) or x == 0:
-        print(end='')
-    else:
-        print(x, end='')
-    for y in range(x - 1):
-        print("-", end='')
-    print (" ")
+print("")
+   
+for row in range(numRow):
+    for x in range(len(str)):
+        
+        if x%(totCycle)==0:
+            if x!= 0:
+                print(x - int(row), end='')
+                second_space(row)
+            print(x + int(row), end='')
+            first_space(row, numRow)
+    print("")
     
-
-print (" ")
-print ("Done")
-
-# function to print first element then .remove() it
-# call funtion everytime 'x' is printed
-# replace '-' to ' '
-# place all loops within another loop
-#       to make sure the list has more elements
+print("")
+print("Done")
