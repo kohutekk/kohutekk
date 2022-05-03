f7569b8f9389ec60c7327e91b34fbc4b27e18c95
kohutekk <104512863+kohutekk@users.noreply.github.com>
2022-05-02 21:43:24 -0500

    Update ZigZagConversion.py

--- Practice_Python/ZigZagConversion.py
+++ Practice_Python/ZigZagConversion.py
@@ -3,13 +3,26 @@
 str = list(input("Enter in a String: "))
 numRow = int(input("Enter number of rows: "))
 totCycle = numRow + (numRow - 2)
+print (" ")
 
 for x in range(numRow):
-    print(str[x], end='')
-    for y in range(numRow - 2):
-        print(" ", end='')
+    print(x, end='')
+    for y in range(x, numRow - 2):
+        print("-", end='')
+    if x == (numRow-1) or x == 0:
+        print(end='')
+    else:
+        print(x, end='')
+    for y in range(x - 1):
+        print("-", end='')
+    print (" ")
     
 
 print (" ")
-print (str)
-print (numRow, " - ", totCycle)
+print ("Done")
+
+# function to print first element then .remove() it
+# call funtion everytime 'x' is printed
+# replace '-' to ' '
+# place all loops within another loop
+#       to make sure the list has more elements
