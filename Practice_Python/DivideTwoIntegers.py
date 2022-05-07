93291b0f87a4107ca468a716fe7bbbb4c85944d4
kohutekk <104512863+kohutekk@users.noreply.github.com>
2022-05-07 17:51:36 -0500

    Update DivideTwoIntegers.py

--- Practice_Python/DivideTwoIntegers.py
+++ Practice_Python/DivideTwoIntegers.py
@@ -1,11 +1,12 @@
 ## Divide Two Integers
 
-num1 = (input("Enter the dividend: ")).split()
-num2 = (input("Enter the divisor: ")).split()
+num1 = list(input("Enter the dividend: "))
+num2 = list(input("Enter the divisor: "))
 
 print(num1)
 
 for x in range(len(num1)):
+    print(x)
     divident = "".join(num1[0])
     num1.remove(num1[0])
 
