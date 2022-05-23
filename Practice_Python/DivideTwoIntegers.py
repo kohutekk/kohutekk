5125dcc669039b14b6a0caf5bbf9bb45106b3bc5
kohutekk <104512863+kohutekk@users.noreply.github.com>
2022-05-23 17:14:23 -0500

    Update DivideTwoIntegers.py

--- Practice_Python/DivideTwoIntegers.py
+++ Practice_Python/DivideTwoIntegers.py
@@ -16,7 +16,6 @@ def sub(num1, num2, ans):
 
 #---------------------------------------------------------------------------------------------------------
 
-
 #defining
 num1 = list(input("Enter the dividend: "))
 num2 = input("Enter the divisor: ")
@@ -24,7 +23,7 @@ length = len(list(num2))
 num2 = int(num2)
 ans = 0
 
-#settint up and first subtraction 
+#setting up and first subtraction 
 divident = num1[0]
 for x in range(1, length):
     function(divident, num1)
@@ -38,19 +37,11 @@ while len(num1) > 1:
     function(divident, num1)
     ans = (ans * 10) + sub(num1, num2, ans)
 
-#---------------------------------------------------------------------------------------------------------
-
-#first subtraction
-sub(num1, num2, qu)
-quotent = qu
-print(qu, " - ",num1)
-
-#loop statment that ends when num1 ends (x is placec in quotent list)
-while len(num1) > 1:
-    moving(divident, num1)
-    sub(num1, num2, qu)
-    quotent = (quotent * 10) + qu
-
-print("The quotent is: ", quotent)
-print("The remainder is: ", num1[0])
+#formating 
+print("----------------------------------------------------------------------------") 
+if int(num1[0]) == 0:
+    num1[0] = '0'
+    
 #use num1[0] as remainder and quotent is the answer
+print("The quotent is: ", ans)
+print("The remainder is: ", num1[0])
