2a4fc786fcd1b0d101d0f3b6b605584fe4b24f9a
kohutekk <104512863+kohutekk@users.noreply.github.com>
2022-05-24 16:03:15 -0500

    Update MergeSortedList.py

--- Practice_Python/MergeSortedList.py
+++ Practice_Python/MergeSortedList.py
@@ -9,21 +9,16 @@ list2 = initiation()
 list3 = initiation()
 anslen = len(list1) + len(list2) + len(list3)
 ans = []
-
-print(anslen)
-print(list1)
-print(list2)
-print(list3)
-
-for x in range(anslen):
-    print("Start")
     
-    if list1[0] < list2[0] and list1[0] < list3[0]:
-        print("C", list1[0])
+for x in range(anslen):
+    print(list1[0], " - ", list2[0], " - ", list3[0])
+    
+    if list1[0] <= list2[0] and list1[0] <= list3[0]:
+        print("A", list1[0])
         ans.append(list1[0])
         list1.remove(list1[0])
-    elif list2[0] < list1[0] and list2[0] < list3[0]:
-        print("C", list2[0])
+    elif list2[0] < list1[0] and list2[0] <= list3[0]:
+        print("B", list2[0])
         ans.append(list2[0])
         list2.remove(list2[0])
     else:
