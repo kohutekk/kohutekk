7bbff508aafd34afe1953a586a0c1152a7549c52
kohutekk <104512863+kohutekk@users.noreply.github.com>
2022-05-24 15:56:52 -0500

    Update MergeSortedList.py

--- Practice_Python/MergeSortedList.py
+++ Practice_Python/MergeSortedList.py
@@ -17,21 +17,16 @@ print(list3)
 
 for x in range(anslen):
     print("Start")
-    if list1[0] > list2[0]:
-        if list2[0] < list3[0]:
-            print("A", list2[0])
-            ans.add(list2[0])
-            list2.remove(list2[0])
-        else:
-            print("B", list3[0])
-            ans.add(list3[3])
-            list3.remove(list3[0])
-    if list1[0] < list2[0]:
-        if list1[0] < list3[0]:
-            print("C", list1[0])
-            ans.add(list1[0])
-            list1.remove(list1[0])
-        else:
-            print("D", list3[0])
-            ans.add(list3[0])
-            list3.remove(list3[0])
+    
+    if list1[0] < list2[0] and list1[0] < list3[0]:
+        print("C", list1[0])
+        ans.append(list1[0])
+        list1.remove(list1[0])
+    elif list2[0] < list1[0] and list2[0] < list3[0]:
+        print("C", list2[0])
+        ans.append(list2[0])
+        list2.remove(list2[0])
+    else:
+        print("C", list3[0])
+        ans.append(list3[0])
+        list3.remove(list3[0])
