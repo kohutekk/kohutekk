2a93ab3e27d3358a79a639f1cc0ee5ac22cfebfa
kohutekk <104512863+kohutekk@users.noreply.github.com>
2022-05-25 10:33:50 -0500

    Update MergeSortedList.py

--- Practice_Python/MergeSortedList.py
+++ Practice_Python/MergeSortedList.py
@@ -26,24 +26,24 @@ anslen = len(list1) + len(list2) + len(list3)
 ans = []
     
 for x in range(anslen):
-    if list1 is none:
-        if list2 is none:
+    if list1 is None:
+        if list2 is None:
             #null2(list3)
             print("null2")
-        elif list3 is none:
+        elif list3 is None:
             #null2(list2)
             print("null2")
         else:
             print("null1")
             null1(list2, list3)
-    elif list2 is none:
-        if list3 is none:
+    elif list2 is None:
+        if list3 is None:
             #null2(list1)
             print("null2")
         else:
             print("null1")
             null1(list1, list3)
-    elif list3 is none:
+    elif list3 is None:
         print("null1")
         null1(list3)
     else:
