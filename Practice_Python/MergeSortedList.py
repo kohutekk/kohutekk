c8cbeef11fb3b456e9f081169968d8679c9653b1
kohutekk <104512863+kohutekk@users.noreply.github.com>
2022-05-25 10:10:24 -0500

    Update MergeSortedList.py

--- Practice_Python/MergeSortedList.py
+++ Practice_Python/MergeSortedList.py
@@ -3,6 +3,21 @@
 def initiation():
     str = input("Enter list: ")
     return list(map(int, str.split(" ")))
+    
+def main():
+    print(list1[0], " - ", list2[0], " - ", list3[0])
+    if list1[0] <= list2[0] and list1[0] <= list3[0]:
+        print("A")
+        ans.append(list1[0])
+        list1.remove(list1[0])
+    elif list2[0] < list1[0] and list2[0] <= list3[0]:
+        print("B")
+        ans.append(list2[0])
+        list2.remove(list2[0])
+    else:
+        print("C")
+        ans.append(list3[0])
+        list3.remove(list3[0])
 
 list1 = initiation()
 list2 = initiation()
@@ -11,14 +26,4 @@ anslen = len(list1) + len(list2) + len(list3)
 ans = []
     
 for x in range(anslen):
-    
-    print(list1[0], " - ", list2[0], " - ", list3[0])
-    if list1[0] <= list2[0] and list1[0] <= list3[0]:
-        ans.append(list1[0])
-        list1.remove(list1[0])
-    elif list2[0] < list1[0] and list2[0] <= list3[0]:
-        ans.append(list2[0])
-        list2.remove(list2[0])
-    else:
-        ans.append(list3[0])
-        list3.remove(list3[0])
+    main()
