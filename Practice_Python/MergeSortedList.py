e646323205f54cf7a56e09f2e418439820d54038
kohutekk <104512863+kohutekk@users.noreply.github.com>
2022-05-26 10:12:02 -0500

    Update MergeSortedList.py

--- Practice_Python/MergeSortedList.py
+++ Practice_Python/MergeSortedList.py
@@ -5,6 +5,7 @@ def initiation():
     return list(map(int, str.split(" ")))
     
 def null0():
+    print(list1[0], " - ", list2[0], " - ", list3[0])
     if list1[0] <= list2[0] and list1[0] <= list3[0]:
         ans.append(list1[0])
         list1.remove(list1[0])
@@ -18,11 +19,9 @@ def null0():
 def null1(listA, listB):
     print(listA[0], " - ", listB[0])
     if listA[0] <= listB[0]:
-        print("1;A")
         ans.append(listA[0])
         listA.remove(listA[0])
     else:
-        print("1;B")
         ans.append(listB[0])
         listB.remove(listB[0])
 
@@ -34,27 +33,24 @@ ans = []
     
 for x in range(anslen):
     print("-------------------------")
-    print(list1[0], " - ", list2[0], " - ", list3[0])
-    if list1[0] is None:
-        print("list1 is null")
-        if list2[0] is None:
+    if not list1:
+        if not list2:
             #null2(list3)
             print("null2")
-        elif list3[0] is None:
+        elif not list3:
             #null2(list2)
             print("null2")
         else:
             print("null1")
             null1(list2, list3)
-    elif list2[0] is None:
-        print("list2 is null")
-        if list3[0] is None:
+    elif not list2:
+        if not list3:
             #null2(list1)
             print("null2")
         else:
             print("null1")
             null1(list1, list3)
-    elif list3[0] is None:
+    elif not list3:
         print("null1")
         null1(list3)
     else:
